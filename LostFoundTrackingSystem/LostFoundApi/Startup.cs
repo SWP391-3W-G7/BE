b8a0ed2dece892ac7a18847416114ed23994887e
Phat-Hy <hyphat2942004@gmail.com>
2025-12-09 02:06:49 +0700

    update yml

---
+++ LostFoundTrackingSystem/LostFoundApi/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace LostFoundApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+        }
+    }
+}
