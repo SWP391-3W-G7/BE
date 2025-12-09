9446890c6613beb7801976a35a769d6dc4c52971
Phat-Hy <hyphat2942004@gmail.com>
2025-12-09 21:13:18 +0700

    update uml

--- LostFoundTrackingSystem/LostFoundApi/Program.cs
+++ LostFoundTrackingSystem/LostFoundApi/Program.cs
@@ -62,14 +62,23 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8081";
-builder.WebHost.UseUrls($"http://*:{port}");
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8082";
+    serverOptions.ListenAnyIP(int.Parse(port));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "LostFound API V1");
+        c.RoutePrefix = "swagger";
+    });
     app.UseHttpsRedirection();
 }
 
