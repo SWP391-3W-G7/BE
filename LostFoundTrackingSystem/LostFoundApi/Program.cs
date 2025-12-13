c54bf8c126b9dab48f3d848ce6f0bc19a85c223b
Phat-Hy <hyphat2942004@gmail.com>
2025-12-13 19:16:07 +0700

    update program

--- LostFoundTrackingSystem/LostFoundApi/Program.cs
+++ LostFoundTrackingSystem/LostFoundApi/Program.cs
@@ -103,8 +103,7 @@ builder.Services.AddCors(options =>
     {
         policy.AllowAnyOrigin()
               .AllowAnyMethod()
-              .AllowAnyHeader()
-              .AllowCredentials();
+              .AllowAnyHeader();
     });
 });
 
